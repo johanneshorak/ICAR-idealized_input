43e3f4379abe44b5cc388d58027dc71663bce7fc
Johannes Horak <johannes.horak@timaios.org>
2019-01-25 05:39:37 -0700

    command line options and derived parameters are now exported to file as wellon one hand this documents the options chosen and on the other hand makes it easier to create the same forcing and topography again

--- idealized_input.py
+++ idealized_input.py
@@ -16,6 +16,7 @@ import sys
 import os as os
 import numpy as np
 import random
+from bunch import Bunch
 from math import radians, cos, sin, asin, sqrt
 from datetime import datetime,timedelta
 
@@ -288,6 +289,7 @@ ws=None
 ws_angle=None
 ws_profile=None
 Nz=None
+Nzf=None    # no. of vertical levels of forcing
 ztop=None
 Nbv=None
 Nbvconst=False
@@ -297,10 +299,13 @@ dlatf=None
 outdir='./ideal_output/'    # if specified ICAR options file and jobscript file will be created
 prepsim=False  # set to true if outdir is specified in command line options
 nzicar=None    # number of vertical levels that ICAR simulation should use
+icaropt=None
+dtf=None       # forcing timestep
 
 # READ COMMAND LINE OPTIONS
+options=Bunch()
 try:
-	opts, args = getopt.getopt(sys.argv[1:],"",["dlon=","dlat=","dx=","dy=","Lx=","Ly=","Llon=","Llat=","a0=","a1=","topo=","ws=","ws_angle=","Nz=","ztop=","Nbv=","rh=","dlonf=","dlatf=","outdir=","nzicar="])
+	opts, args = getopt.getopt(sys.argv[1:],"",["dlon=","dlat=","dx=","dy=","Lx=","Ly=","Llon=","Llat=","a0=","a1=","topo=","ws=","ws_angle=","Nz=","Nzf=", "ztop=","Nbv=","rh=","dlonf=","dlatf=","outdir=","nzicar=","icaropt=","dtf="])
 	if len(opts)==0:
 		print_help()
 		sys.exit(1)
@@ -309,6 +314,7 @@ except getopt.GetoptError:
 	sys.exit(2)
 for opt, arg in opts:
 	#print opt," ",arg
+	options[opt.replace('--','')]=arg
 	if opt in ("-h","--help"):
 		print_help()
 		sys.exit()
@@ -356,6 +362,12 @@ for opt, arg in opts:
 		prepsim = True
 	elif opt in ("--nzicar"):
 		nzicar = int(arg)
+	elif opt in ("--Nzf"):
+		Nzf = int(arg)
+	elif opt in ("--icaropt"):
+		icaropt=arg
+	elif opt in ("--dtf"):
+		dtf=int(arg)
 
 # dlon		... longitudinal resolution in degrees
 # dlat		... latitudinal resolution in degrees
@@ -423,11 +435,14 @@ if not(dlon is None) and not(dlat is None) and not(Llon is None) and not(Llat is
 	dy = haversine(lonc,latc-dlat/2.0,lonc,latc+dlat/2.0)
 	Lx = haversine(lonc-Llon/2.0,latc,lonc+Llon/2.0,latc)
 	Ly = haversine(lonc,latc-Llat/2.0,lonc,latc+Llat/2.0)
+	options["dx"] = dx
+	options["dy"] = dy
+	options["Lx"] = Lx
+	options["Ly"] = Ly
 elif not(dx is None) and not(dy is None) and not (Lx is None) and not (Ly is None):
 	dg_lon = haversine(lonc-0.5,latc,lonc+0.5,latc)			# yields km per degree of longitude (km/deg)
 	dg_lat = haversine(lonc,latc-0.5,lonc,latc+0.5)			# yields km per degree of latitude  (km/deg)
 	# find Llon and Llat
-	
 
 	# find longitudinal extension by calling haversine until
 	# we find the angle that best approximates the distance
@@ -454,6 +469,11 @@ elif not(dx is None) and not(dy is None) and not (Lx is None) and not (Ly is Non
 
 	dlon = 1.0/dg_lon
 	dlat = 1.0/dg_lat
+
+	options["Llon"] = Llon
+	options["Llat"] = Llat
+	options["dlon"] = dlon
+	options["dlat"] = dlat
 else:
 	print(" error: either (dlon,dlat) and (Llon,Llat) or (dx,dy) and (Lx,Ly) must be specified!")
 	sys.exit(1)
@@ -461,34 +481,47 @@ else:
 if ws_profile is None:
 	print("    {:20s}: not supplied, setting windspeed constant with height".format("ws-z-profile"))
 	ws_profile='const'
+	options["ws_profile"] = ws_profile
 	
 if ws is None:
 	print("    {:20s}: no backgrond windspeed supplied, using ws=10m/s".format("ws"))
 	ws = 10.0
+	options["ws"] = ws
 	
 if ws_angle is None:
 	print("    {:20s}: no angle supplied, using ws_angle=270.0 degree (westwind)".format("ws_angle"))
 	ws_angle_0	= 270.0
 	ws_angle	= ws_angle_0+90.0		# add 90 degrees so that sin and cos yield values that match the orientation (N..0,E..90,S..180,W..270)
+	options["ws_angle"] = ws_angle
 else:
 	ws_angle_0	= ws_angle
 	ws_angle	+=90.0				# add 90 degrees so that sin and cos yield values that match the orientation (N..0,E..90,S..180,W..270)
-
+	options["ws_angle"] = ws_angle
 if Nz is None:
 	print("    {:20s}: number of z-levels not supplied, using Nz=30".format("Nz"))
 	Nz = 30
-
+	options["Nz"] = Nz
+if Nzf is None:
+	print("    {:20s}: number of forcing z-levels not supplied, setting Nzf = Nz/3".format("Nzf"))
+	Nzf = int(Nz/3)
+	options["Nzf"] = Nzf
+if dtf is None:
+	print("    {:20s}: forcing timestep not supplied, setting dtf = 6h".format("dtf"))
+	dtf=6
+	options["dtf"] = dtf
 if dlonf is None:
 	dlonf = dlon
-
+	options["dlonf"] = dlonf
 	
 if dlatf is None:
 	dlatf = dlat
+	options["dlatf"] = dlatf
 
 if ztop is None:
 	print("    {:20s}: top level height above topography not supplied, using ztop=40km".format("ztop"))
 	ztop = 40.0
-	
+	options["ztop"] = ztop	
+
 if Nbvconst == True:
 	print("    {:20s}: calculating potential temperature so that N =={:8.4f} is constant".format("N",Nbv))
 else:
@@ -521,6 +554,8 @@ if Nlat % 2 == 0:										# see above
 # calculate grid-spacing in km for FORCING
 dxf = haversine(lonc - dlonf / 2.0, latc, lonc + dlonf / 2.0, latc)
 dyf = haversine(lonc, latc - dlatf / 2.0, lonc, latc + dlatf / 2.0)
+options["dxf"] = dxf
+options["dyf"] = dyf
 
 print("* Generating high resolution topography for ICAR experiment")
 print("-----------------------------------------------------------------")
@@ -607,7 +642,7 @@ Nlon, Nlat							= get_no_of_gridcells(Llon,dlon,Llat,dlat)
 lonN,latN,lon_gridded,lat_gridded	= generate_grid(Nlon, dlon, Nlat, dlat)
 
 Ndays		= 1 						# number of days for which forcing should be generated
-Ntime		= Ndays*24
+Ntime		= int((Ndays*24)/dtf)
 
 dtime_base  	  = datetime(1900,1,1,0,0,0)
 dtime_start 	  = datetime(2017,1,1,0,0,0)
@@ -616,7 +651,7 @@ dtime_diff_start  = (dtime_start-dtime_base)
 dtime_diff_end	  = (dtime_end-dtime_base)
 hours_since_start = dtime_diff_start.days*24.0	+ dtime_diff_start.seconds/3600.0
 hours_since_end	  = dtime_diff_end.days*24.0	+ dtime_diff_end.seconds/3600.0
-time_list	= np.arange(hours_since_start,hours_since_end,1.0)
+time_list	= np.arange(hours_since_start,hours_since_end,dtf)
 
 i_Time		= time_list
 i_hgt 		= np.empty(Ntime*Nlat*Nlon).reshape(Ntime,Nlat,Nlon)
@@ -766,13 +801,19 @@ if prepsim:
 		]
 	)
 	# read, adjust and write icar options file
-	with open('./data/cheyenne.icar-095.snz_6a.options', 'r') as content_file:
+	with open(icaropt, 'r') as content_file:
 		content = content_file.read()
 	content_file.close()
 
 	for n in range(len(subst_df)):
 		row = subst_df.iloc[n]
 		content = content.replace(row['key'], str(row['value']))
+
+	# write all the options used to create input and forcing to file
+	with open("{:s}/gii.options".format(outdir), "w") as content_file:
+		for key in options:
+			content_file.write('{:s} = {:s}\n'.format(key,str(options[key])))
+	content_file.close()
 
 	with open("{:s}/icar.options".format(outdir), "w") as content_file:
 		content_file.write(content)
